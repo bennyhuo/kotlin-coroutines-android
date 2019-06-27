e3363eb9a4f5da20985718955584422136427df6
bennyhuo <bennyhuo@kotliner.cn>
2019-06-27 21:56:21 +0800

    add support for lifecycle.

---
+++ MainScope/src/main/java/com/bennyhuo/kotlin/coroutines/android/mainscope/lifecycle/AndroidXLifecycleOwnerSupport.kt
@@ -0,0 +1,32 @@
+package com.bennyhuo.kotlin.coroutines.android.mainscope.lifecycle
+
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleRegistry
+import com.bennyhuo.kotlin.coroutines.android.mainscope.EmptyScope
+import com.bennyhuo.kotlin.coroutines.android.mainscope.MainScope
+import com.bennyhuo.kotlin.coroutines.android.mainscope.exception.UnsupportedTypeException
+import com.bennyhuo.kotlin.coroutines.android.mainscope.scope.MainScoped
+import com.bennyhuo.kotlin.coroutines.android.mainscope.utils.Logcat
+import java.lang.ref.WeakReference
+
+private val lifecycleOwnerField by lazy {
+    try {
+        LifecycleRegistry::class.java.getDeclaredField("mLifecycleOwner").also { it.isAccessible = true }
+    } catch (e: Exception) {
+        Logcat.warn("Cannot retrieve property: mLifecycleOwner from AndroidX LifecycleRegistry.")
+        null
+    }
+}
+
+val Lifecycle.mainScope: MainScope
+    get() {
+        return if(this is LifecycleRegistry) mainScope else {
+            Logcat.warn("Only lifecycles of Activity or Fragment are supported.")
+            throw UnsupportedTypeException(this::class.java, "LifecycleRegistry")
+        }
+    }
+
+val LifecycleRegistry.mainScope: MainScope
+    get() {
+        return ((lifecycleOwnerField?.get(this) as? WeakReference<*>)?.get() as? MainScoped)?.mainScope ?: EmptyScope
+    }
--- app-androidx-fragment/src/main/java/com/bennyhuo/kotlin/coroutines/android/sample/MainActivity.kt
+++ app-androidx-fragment/src/main/java/com/bennyhuo/kotlin/coroutines/android/sample/MainActivity.kt
@@ -2,8 +2,11 @@ package com.bennyhuo.kotlin.coroutines.android.sample
 
 import android.os.Bundle
 import androidx.fragment.app.FragmentActivity
+import com.bennyhuo.kotlin.coroutines.android.mainscope.lifecycle.mainScope
 import com.bennyhuo.kotlin.coroutines.android.mainscope.scope.BasicScoped
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : FragmentActivity(), BasicScoped {
 
@@ -16,6 +19,15 @@ class MainActivity : FragmentActivity(), BasicScoped {
                     .setCustomAnimations(android.R.anim.fade_in,android.R.anim.fade_out)
                     .add(R.id.fragmentContainer, MainFragment())
                     .addToBackStack("Main").commit()
+        }
+
+
+        textView.onClick {
+            lifecycle.mainScope.launch {
+                log(1)
+                delay(1000)
+                log(2)
+            }
         }
     }
 
